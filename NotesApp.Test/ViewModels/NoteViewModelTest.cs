c28c394bc138a4f5d602bb3ca6d4eff269206759
adriana634 <50183872+adriana634@users.noreply.github.com>
2023-04-25 21:43:56 +0200

    NoteViewModel unit tests

---
+++ NotesApp.Test/ViewModels/NoteViewModelTest.cs
@@ -0,0 +1,39 @@
+﻿using FluentAssertions;
+using Moq;
+using NotesApp.Models;
+using NotesApp.ViewModels;
+
+namespace NotesApp.Test.ViewModels
+{
+    public class NoteViewModelTest
+    {
+        [Fact]
+        public void Refresh_ShouldRaisePropertyChangedEvent()
+        {
+            // Arrange
+            var noteMock = new Mock<Note>();
+            var viewModel = new NoteViewModel(noteMock.Object);
+
+            var textPropertyChangedRaised = false;
+            var datePropertyChangedRaised = false;
+            viewModel.PropertyChanged += (sender, args) =>
+            {
+                if (args.PropertyName == nameof(NoteViewModel.Text))
+                {
+                    textPropertyChangedRaised = true;
+                } 
+                else if (args.PropertyName == nameof(NoteViewModel.Date))
+                {
+                    datePropertyChangedRaised = true;
+                }
+            };
+
+            // Act
+            viewModel.Refresh();
+
+            // Assert
+            textPropertyChangedRaised.Should().BeTrue();
+            datePropertyChangedRaised.Should().BeTrue();
+        }
+    }
+}
